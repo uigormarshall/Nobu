53270d0ddcc6735e5087028c3f291972e4391050
uigormarshall <uigor.silva@acad.ifma.edu.br>
2023-05-04 18:27:31 -0300

    feat: implementa logica de pulo

--- game/Assets/Characters/PlayerController.cs
+++ game/Assets/Characters/PlayerController.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 0.7f;
+    public float jumpForce = 5f;
+    public int doubleJumpCount = 0;
     public Transform skin;
+    private Rigidbody2D rb;
+
     private void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     private void Update()
@@ -16,6 +20,7 @@ public class PlayerController : MonoBehaviour
         float moveInput = Input.GetAxisRaw("Horizontal"); // Obtém a entrada horizontal do jogador
         transform.position += new Vector3(moveInput * speed * Time.deltaTime, 0f, 0f); // Move o personagem na direção horizontal
         run();
+        jump();
     }
 
     private void run(){
@@ -25,5 +30,18 @@ public class PlayerController : MonoBehaviour
         }else{
             skin.GetComponent<Animator>().SetBool("PlayerRunning", false);
         }
+    }
+
+    private void jump(){
+        if (Input.GetButtonDown("Jump") && NotJumping() )
+        {
+            rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+        }
+    }
+    private bool NotJumping(){
+        if(Mathf.Abs(rb.velocity.y) < 0.001f){
+            return true;
+        }
+        return false;
     }
 }
